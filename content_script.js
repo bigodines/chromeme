ed6683a0c557c92baa8827a5c9d67f53d023a9a7
Matheus Mendes <matheus@yahoo-inc.com>
2010-10-07 17:25:21 -0300

    refactored command fuctions, added a bind and unbind to enable/disable highlight

--- content_script.js
+++ content_script.js
@@ -2,28 +2,37 @@ chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
       var content, postUrl, params;
       if (request.cmd == "clear") {
-   	  smart_meme.clearSelection();
+          smart_meme.clearSelection();
 	  
       } else if (request.cmd == "post") {
-	  content = smart_meme.getContent();
-	  params = '';
-	  if (content.post_type && content.post_type != "text") { 
-	      params +="post_type="+content.post_type+"&content="+content.src;
-	      params += "&caption="+encodeURIComponent(content.join(''));
-	  } else {
-	      params += "post_type=text&content="+encodeURIComponent(content.join(''));
-	  }
-	  url = 'http://www.bigodines.com/meme-php/examples/oAuthExample.php';
-	  sendResponse({url:url, 
-		      data:params});
+          content = smart_meme.getContent();
+          params = '';
+          if (content.post_type && content.post_type != "text") { 
+              params +="post_type="+content.post_type+"&content="+content.src;
+              params += "&caption="+encodeURIComponent(content.join(''));
+          } else {
+              params += "post_type=text&content="+encodeURIComponent(content.join(''));
+          }
+          url = 'http://www.bigodines.com/meme-php/examples/oAuthExample.php';
+          sendResponse({url:url, 
+                      data:params});
 	  
+      } else if (request.cmd =="bind"){ 
+          document.body.addEventListener("mousemove", move_inspector, true);          
+      } else if (request.cmd == "unbind") {
+          smart_meme.clearSelection();
+          document.body.removeEventListener("mousemove", move_inspector, true);
       } else {
-	  sendResponse({}); // snub them.
+          sendResponse({}); // snub them.
       }
   }
 );
 
-document.body.onmousemove = function(e) { 
-    
+
+move_inspector = function(e) { 
     smart_meme.eventHandler(e);
 };
+
+//document.body.onmousemove = move_inspector;
+
+
